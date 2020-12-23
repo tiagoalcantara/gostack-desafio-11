75e25692af32ca7eb240083ef90604506ffd839c
Tiago Alcantara <tiagofacampos@gmail.com>
2020-12-23 17:22:06 -0300

    passed tests

--- src/pages/FoodDetails/index.tsx
+++ src/pages/FoodDetails/index.tsx
@@ -73,34 +73,69 @@ const FoodDetails: React.FC = () => {
 
   useEffect(() => {
     async function loadFood(): Promise<void> {
-      // Load a specific food with extras based on routeParams id
+      const { data } = await api.get(`/foods/${routeParams.id}`);
+      setFood({
+        ...data,
+        formattedPrice: formatValue(data.price)
+      });
+
+      setExtras(data.extras.map((extra: Omit<Extra, 'quantity'>) => ({...extra, quantity: 0})));
     }
 
     loadFood();
   }, [routeParams]);
 
   function handleIncrementExtra(id: number): void {
-    // Increment extra quantity
+    const arrayClone = [...extras];
+    const findIndex = arrayClone.findIndex(extra => extra.id === id);
+
+    arrayClone[findIndex].quantity += 1;
+
+    setExtras(arrayClone);
   }
 
   function handleDecrementExtra(id: number): void {
-    // Decrement extra quantity
+    const arrayClone = [...extras];
+    const findIndex = arrayClone.findIndex(extra => extra.id === id);
+
+    if(arrayClone[findIndex].quantity > 0) {
+      arrayClone[findIndex].quantity -= 1;
+    }
+
+    setExtras(arrayClone);
   }
 
   function handleIncrementFood(): void {
-    // Increment food quantity
+    setFoodQuantity(value => value + 1);
   }
 
   function handleDecrementFood(): void {
-    // Decrement food quantity
+    setFoodQuantity(value => {
+      if(value === 1) return 1;
+
+      return value - 1;
+    });
   }
 
   const toggleFavorite = useCallback(() => {
-    // Toggle if food is favorite or not
+    if(isFavorite) {
+      api.delete(`/favorites/${food.id}`);
+    } else {
+      api.post('/favorites', food);
+    }
+
+    setIsFavorite(!isFavorite);
   }, [isFavorite, food]);
 
   const cartTotal = useMemo(() => {
-    // Calculate cartTotal
+    const extrasPrice = extras.reduce((acc, extra) => {
+      acc += extra.value * extra.quantity;
+      return acc;
+    }, 0)
+
+    const value = (food.price + extrasPrice) * foodQuantity;
+
+    return formatValue(value);
   }, [extras, food, foodQuantity]);
 
   async function handleFinishOrder(): Promise<void> {
